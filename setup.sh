7390c0623c4d9a5bbfea49e3c322739c8767a7a5
zackn9ne <zackn9ne@gmail.com>
2017-04-28 13:02:39 -0400

    reasons

--- setup.sh
+++ setup.sh
@@ -1,12 +1,25 @@
-
 #!/bin/sh
 MODULES=~/.newsbeuter/modules
+MODULESRLF=~/.newsbeuter/modules/readlater
 READLATER=readlater.sh
 
-echo "running newsbeuter setup..."
-echo "making modules dir.."
+echo "running newsbeuter setup..." 
+
+
+if [ ! -d "$MODULES" ]; 
+then 
+echo "making modules dir.." 
 mkdir $MODULES
-touch $MODULES/data
-echo "recalculating splines..."
-sudo chmod +x $READLATER
-cp $READLATER $MODULES
+  # Control will enter here if $DIRECTORY exists.
+fi
+if [ ! -d "$MODULESRLF" ]; 
+then 
+echo "making modules dir.." 
+mkdir $MODULESRLF
+  # Control will enter here if $DIRECTORY exists.
+fi
+
+echo "modules directory found making datafile..."
+touch $MODULESRLF/data 
+echo "recalculating splines..." 
+cp $READLATER $MODULESRLF
